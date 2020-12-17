012d8f361db48ed5773e5f08a8678a639928a290
CloudComputingPirate <wazirshubham@outlook.com>
2020-12-17 14:07:59 +0530

    Second commit

--- hello.py
+++ hello.py
@@ -5,8 +5,8 @@ import click
 @click.option("--color", prompt="I need your color", help="This is your color")
 def hello(name, color):
     if name == "Thor":
-        click.echo("Thor, you are always red.")
-        click.echo(click.style(f"Hello {name}!", fg="red"))
+        click.echo("Thor, you are always blue.")
+        click.echo(click.style(f"Hello {name}!", fg="blue"))
     else:
         click.echo(f"Your color is {color}!")
         click.echo(click.style(f"Hello {name}!", fg=color))
