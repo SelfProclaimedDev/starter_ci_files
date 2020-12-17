28cd2af9aa0b6d9db68f86c70d33093c3b6184d4
CloudComputingPirate <wazirshubham@outlook.com>
2020-12-17 15:10:00 +0530

    Pehla commit

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ import click
 
 @click.command(help="This is just a hello app. It does nothing.")
 @click.option("--name", prompt="I need your name", help="Need your name")
-@click.option("--color", prompt="I need your color", help="This is your color of choice")
+@click.option("--color", prompt="I need your color", help="This is your color of choices")
 def hello(name, color):
     if name == "Thor":
         click.echo("Thor, you are always blue.")
